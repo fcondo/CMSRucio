8cf4540b80e965e49703faccab25bc046cc75b5a
Fabio Condomitti <facondom@lxplus761.cern.ch>
2019-09-10 17:42:49 +0200

    username is now the key of the users' dictionary

--- src/user_to_site_mapping.py
+++ src/user_to_site_mapping.py
@@ -1,9 +1,15 @@
-from rucio.client import Client
-from rucio.common.exception import RSENotFound, RSEOverQuota, AccountNotFound
+#! /usr/bin/env python
 
+from rucio.client import Client
+from rucio.common.exception import RSENotFound, AccountNotFound, AccessDenied, RSEOperationNotSupported
+# from rucio.api.permission import has_permission
+
+import os
 import sys
 import json
 import urllib2
+
+import utility as util
 
 # rucio Client
 client = Client()
@@ -11,93 +17,74 @@ client = Client()
 # dictionary with US-based only CRIC users, their RSEs and the corresponding quotas
 cric_user = {}
 
-DEFAULT_RSE_SIZE = 1  # Tb
 
-# dictionary of Tier-2's dictionaries grouped by main country, needed for the user-to-site mapping
-rses_by_country = {
-    "US": {
-        'T2_US_Caltech': ['California Institute of Technology', 'Lawrence Livermore Nat. Laboratory',
-                          'University of California Davis', 'University of California  Los Angeles'],
-
-        'T2_US_Florida': ['Florida International University', 'Florida State University',
-                          'Florida Institute of Technology', 'University of Florida', 'University of Puerto Rico'],
-
-        'T2_US_MIT': ['Boston University', 'Brown University', 'Fairfield University',
-                      'Massachusetts Inst. of Technology', 'Northeastern University'],
-
-        'T2_US_Nebraska': ['University of Colorado Boulder', 'University of Iowa', 'Kansas State University',
-                           'The University of Kansas', 'University of Nebraska Lincoln'],
-
-        'T2_US_Purdue': ['Carnegie-Mellon University', 'Ohio State University',
-                         'Purdue University', 'The State University of New York SUNY'],
-
-        'T2_US_UCSD': ['University of California Riverside', 'Univ. of California Santa Barbara',
-                       'Univ. of California San Diego'],
-
-        'T2_US_Wisconsin': ['University of Minnesota', 'University of Rochester', 'Wayne State University',
-                            'University of Wisconsin Madison'],
-
-        'T1_US_FNAL_Disk': ['Fermi National Accelerator Lab.'],
-
-        'T2_US_Vanderbilt': ['Vanderbilt University'],
-    }
-}
-
-
-# return a site based on the user's institute and institute country
 def mapping_by_country(institute, country):
-    if not institute:  # institute = "", the user no longer belong to CMS Experiment
+    """
+    This function returns a site based on the user's institute and institute country.
+    If institute = '' the user no longer belong to CMS Experiment.
+    """
+    if not institute:
         return
-    # select rse and institutes in the given country
-    institutes_by_country = rses_by_country[country]
+
+    institutes_by_country = util.rses_by_country[country]
 
     for rse_key, institutes_val in institutes_by_country.items():
-        # if the institute belongs to the list, return the RSE = key
         if institute in institutes_val:
             return rse_key + "_Test"
 
 
 def user_to_site_mapping_by_country(country):
-    # get all the CRIC user profiles in JSON format
-    cric_url = 'https://cms-cric.cern.ch/api/accounts/user/query/list/?json'
-    cric_global_user = json.load(urllib2.urlopen(cric_url))
+    """
+    This function gets all the CRIC user profiles in JSON format by using
+    CRIC API. For each user extracts relevant information to run the algorithm.
+    Then update the cric_user dictionary.
+    """
+    cric_global_user = json.load(urllib2.urlopen(util.CRIC_URL))
+    issuer = client.whoami()
+#    if not permission.has_permission(issuer, action='set_limits'):
+ #       raise AccessDenied('Account %s can not set_limits' % (issuer))
 
     for key, user in cric_global_user.items():
         institute_country = user['institute_country'].encode("utf-8")
 
         if country in institute_country:
-            name = key.encode("utf-8")
+            # name = key.encode("utf-8")
             dn = user['dn'].encode("utf-8")
             institute = user['institute'].encode("utf-8")
-            email = user['institute'].encode("utf-8")
-
-            # check for duplicates
-            if name not in cric_user:
-                # utility function
-                tmp = create_dict_entry(name, dn, email, institute, institute_country)
+            email = user['email'].encode("utf-8")
+            username = user['profiles'][0]['login'].encode("utf-8")
+            if username not in cric_user:
+                tmp = create_dict_entry(username, dn, email, institute, institute_country)
                 if not tmp:
                     continue
-                # update the dictionary
+
                 cric_user.update(tmp)
 
 
 # return a dictionary entry
-def create_dict_entry(name, dn, email, institute, institute_country):
-    # obtain the right RSE for the current user based on its institute
+def create_dict_entry(username, dn, email, institute, institute_country):
+    """
+    This utility function obtains the right RSE for the current user based on its institute
+    and build and return a dictionary entry with the correct user to site mapping.
+    Assigns a default quota at that site.
+    """
     rse_key = mapping_by_country(institute, institute_country)
 
     if not rse_key:  # user no longer in CMS
         return
 
+    client.set_account_limit(account=username, rse=rse_key, bytes=util.DEFAULT_RSE_SIZE)
+
     entry = {
-        name: {
+        username: {
             "dn": dn,
             "institute": institute,
             "institute_country": institute_country,
             "email": email,
             "RSES": {
-                rse_key: DEFAULT_RSE_SIZE,
-                'rse_default': 0
+                rse_key: util.DEFAULT_RSE_SIZE,
+                # 'other_rse': 0,
+
             }
         }
     }
@@ -105,100 +92,141 @@ def create_dict_entry(name, dn, email, institute, institute_country):
 
 
 # get user information
-def get_user(name):
+def get_user(username):
     try:
-        info = cric_user[name]
+        info = cric_user[username]
     except KeyError:
         raise AccountNotFound
     return info
 
 
-# get the current quota given a user and one of its RSE
-def get_user_quota(name, rse=None):
-    # check if user is in CRIC
+def get_user_quota(username=None, rse=None, method='get'):
+    """
+    This function returns the quota for a given user at a certain site.
+    """
     try:
-        user_val = cric_user[name]
-    except KeyError:
-        # sys.stderr.write("Account not found in CRIC\n")      
-        raise AccountNotFound
-    
-    if user_val is None:
-        # sys.stderr.write("Account not found in CRIC\n")
-        raise AccountNotFound
-
-    # check if rse is None
-    if rse is None:
-        # sys.stderr.write("RSE is None\n")
-        raise RSENotFound
-
-    # check if user has quota on that RSE
-    if rse not in user_val['RSES']:
-        # sys.stderr.write("User has no quota on this RSE\n")
-        raise RSENotFound
-
-    # check if account exists in Rucio
-    try:
-        account = client.whoami()['account']
-        client.get_account(account)
+        check_user_error(username)
     except AccountNotFound:
-        sys.stderr.write("Account not found in Rucio\n")
         raise
 
-    # chekf it the RSE actually exists
     try:
-        client.get_rse(rse)['rse']
+        check_rse_error(username, rse, method)
     except RSENotFound:
-        sys.stderr.write("RSE not found\n")
-        raise RSENotFound
+        raise
 
-    return user_val['RSES'][rse]
+    limit = client.get_account_limit(account=username, rse=rse)
+    return limit[rse]
 
 
-# set a new quota for a certain RSE for a given user
-def set_user_quota(name, rse, quota):
-    # check if user is in CRIC
+def reset_user_quota(username=None, rse=None):
+    """
+    This function resets a given RSE for a given user to the default value.
+    """
     try:
-        user_val = cric_user[name]
-    except KeyError:
-        raise AccountNotFound
-
-    if user_val is None:
-        # sys.stderr.write("Account not found in CRIC\n")
-        raise AccountNotFound
-
-    # check if account exists in Rucio
-    try:
-        account = client.whoami()['account']
-        client.get_account(account)
+        check_user_error(username)
     except AccountNotFound:
-        sys.stderr.write("Account not found in Rucio\n")
         raise
-    # check if rse is None
-    if rse is None:
-        # sys.stderr.write("RSE is None\n")
-        raise RSENotFound
 
-    # check if the RSE actually exists
     try:
-        client.get_rse(rse)['rse']
+        check_rse_error(username=username, rse=rse, method='get')
     except RSENotFound:
-        # sys.stderr.write("RSE does not exists\n")     
         raise
 
-    # check if user has quota on that RSE
-    if rse not in user_val['RSES']:
-        # sys.stderr.write("User has no quota on this RSE\n")
-        raise RSENotFound
+    cric_user[username]["RSES"][rse] = util.DEFAULT_RSE_SIZE
+    client.set_account_limit(username, rse, util.DEFAULT_RSE_SIZE)
 
-    old_quota = get_user_quota(name, rse)
 
+def set_user_quota(username, rse, quota):
+    """
+    This function sets a new quota for a certain RSE for a given user.
+    """
+    try:
+        check_user_error(username)
+    except AccountNotFound:
+        raise
+
+    try:
+        check_rse_error(username=username, rse=rse, method='set')
+        check_quota_error(username, rse, quota, 'set')
+    except RSENotFound:
+        raise
+    except RSEOperationNotSupported:
+        raise
+
+    cric_user[username]["RSES"][rse] = quota
+    client.set_account_limit(username, rse, quota)
+
+
+def check_quota_error(username=None, rse=None, quota=0, method='get'):
+    """
+    This functions checks if the new given quota is less than the current one or
+    if it has wrong values.
+    """
+    old_quota = get_user_quota(username, rse, method)
     if old_quota is not None and (quota <= 0 or quota < int(old_quota)):
-        template = "Old quota is {0}. Invalid new quota is {1}\n"
-        message = template.format(old_quota, quota)
-        # sys.stderr.write(message) 
-        raise RSEOverQuota
+        raise RSEOperationNotSupported
 
-    user_val["RSES"][rse] = quota
+
+def delete_user_quota(username=None, rse=None):
+    """
+    This function deletes the limits on an rse of a given user.
+    """
+    try:
+        check_user_error(username)
+    except AccountNotFound:
+        raise
+
+    try:
+        check_rse_error(username, rse)
+    except RSENotFound:
+        raise
+
+    del cric_user[username]["RSES"][rse]
+    client.delete_account_limit(username, rse)
+
+
+def check_user_error(username=None):
+    """
+    This function checks if user is in CRIC users.
+    """
+    try:
+        user_val = cric_user[username]
+    except KeyError:
+        raise AccountNotFound
+    try:
+        client.get_account(username)
+    except AccountNotFound:
+        raise
+
+    if user_val is None:
+        raise AccountNotFound
+
+
+def check_rse_error(username=None, rse=None, method='get'):
+    """
+    This function checks for errors on the rse name.
+    Then checks if it the RSE actually exists.
+    Method parameter is used to not check if the current user has
+    quota on a given rse to reuse this function for the set method.
+    """
+    user_val = cric_user[username]
+    if rse is None:
+        raise RSENotFound
+
+    '''if rse not in user_val['RSES'] and method is 'get':
+        print user_val['RSES'].keys(), method
+        raise RSENotFound'''
+
+    if method is 'get':
+        try:
+            user_val['RSES'][rse]
+        except KeyError:
+            raise RSENotFound
+
+    try:
+        client.get_rse(rse)['rse']
+    except RSENotFound:
+        raise
 
 
 user_to_site_mapping_by_country('US')
